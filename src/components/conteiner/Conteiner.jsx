dedec1a2390b9b9cc7bf92d130f3fe2f24093b46
BogdanYeshchenko <119247282+BogdanYeshchenko@users.noreply.github.com>
2023-07-12 20:28:13 +0000

    Deploying to gh-pages from @ BogdanYeshchenko/Tweet-Cards@7216ef9d53f229fbd076543a046b93bf8a9bca1c 🚀

--- src/components/conteiner/Conteiner.jsx
+++
@@ -1,7 +0,0 @@
-import './conteiner.css';
-
-const Conteiner = ({ children }) => {
-  return <div className="conteiner">{children}</div>;
-};
-
-export default Conteiner;
--- src/components/conteiner/ConteinerCenter.jsx
+++
@@ -1,7 +0,0 @@
-import './conteinerCenter.css';
-
-const ConteinerCenter = ({ children }) => {
-  return <div className="conteinerCenter">{children}</div>;
-};
-
-export default ConteinerCenter;
