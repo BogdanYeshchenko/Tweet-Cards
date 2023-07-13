bd571e79f3189b8b1fe0f92d8dd64a061fb1bb3d
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-07-13 17:23:42 +0300

    add 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import "./App.css";
-// import { useDispatch } from "react-redux";
-// import { useEffect } from "react";
-// import { getUsers } from "./redux/users/usersOperations";
-// import { useSelector } from "react-redux";
-// import { useState } from "react";
-// import { changeFollowingStatus } from "./redux/followingStatus/followingStatusSlice";
 import { Route, Routes } from "react-router-dom";
-// import ConteinerCenter from "./components/conteiner/ConteinerCenter";
-// import Conteiner from "./components/conteiner/Conteiner";
-// import TweetCard from "./components/tweetCards/tweetCard";
-// import Button from "./components/button/button";
-// import PulseLoader from "react-spinners/PulseLoader";
 import TweetCardsPage from "./components/page/tweetCardsPage";
 import Layout from "./components/layout/Layout";
 import HomePage from "./components/page/homePage";
