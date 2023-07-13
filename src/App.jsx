3678cee81ca4291603cec2e7400916db3b4ddeaf
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-07-13 19:07:38 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
         </Route>
       </Routes>
-      {/* <Navigate to="/tweets-cards" /> */}
+      <Navigate to="/tweets-cards" />
     </>
   );
 }
--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,23 +1,12 @@
 import "./button.css";
-// import { useState, useEffect } from "react";
 
 const Button = ({ isActive, onClick, text }) => {
-  // const [colorButton, setColorButton] = useState("#EBD8FF");
-
-  // useEffect(() => {
-  //   isActive ? setColorButton("#5CD3A8") : setColorButton("#EBD8FF");
-  // }, [isActive]);
-
-  const colorButtonHover = "#5cd3a8";
-
   return (
     <button
       onClick={onClick}
       type="button"
       style={{
         backgroundColor: isActive ? "#5CD3A8" : "#EBD8FF",
-        // "--colorButton": colorButton,
-        // "--colorButtonHover": colorButtonHover,
       }}
       className="meinButtonBox"
     >
